b7ae3d3d95eddd5682d79649aac26898e40933c5
Obie-Wan <rustyhex@rambler.ru>
2013-07-12 11:57:13 +0400

    distutils install script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='django_hadoop', 
+    description='Django application for running Hadoop Map-Reduce tasks and getting their results',
+    packages=[
+        'django_hadoop',
+    ],
+)
