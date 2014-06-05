3b856cb979e4092709c562a94b1315ad370f040e
Alexandr Trubnikov <trubnikov@corp.sputnik.ru>
2014-06-05 16:49:23 +0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-from distutils.core import setup
 
 setup(
     name='django_hadoop',
     description='Django application for running Hadoop Map-Reduce \
                  tasks and getting their results',
-    packages=[
-        'django_hadoop',
-    ],
     version=u'0.1.0',
     url=u'https://github.com/Obie-Wan/django_hadoop',
     license='LICENSE',
